aef5f9047e942c5b3fc3ca50e35a1b057b372f05
Andrew Olney <aolney@memphis.edu>
2025-06-03 16:49:04 -0500

    conversion first pass, needs runtime checking

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import {
   JupyterFrontEndPlugin,
   LabShell,
 } from "@jupyterlab/application";
+import IRenderMimeRegistry from "@jupyterlab/rendermime";
 import { INotebookTracker, NotebookPanel } from "@jupyterlab/notebook";
 import $ from "jquery";
-import { CodeCellModel, MarkdownCell, MarkdownCellModel } from "@jupyterlab/cells";
+import { CodeCellModel, MarkdownCell, MarkdownCellModel, Cell } from "@jupyterlab/cells";
 import { IDocumentManager } from "@jupyterlab/docmanager";
 
 //TODO notes
@@ -31,9 +32,9 @@ let isInternalLinkGenerated = false;
 // Hide UI elements to effect lockdown
 function lockdown(this: LabShell) {
 
-  if( reloadBlockParam === "1" ){
+  if (reloadBlockParam === "1") {
     let main = document.getElementById("main");
-    if(main) main.style.display = "none"
+    if (main) main.style.display = "none"
     window.alert("You cannot reload this tab.\n\nPlease return to the experiment tab to continue.\n\nYou may close this tab now.");
   }
 
@@ -41,24 +42,22 @@ function lockdown(this: LabShell) {
   this.collapseLeft();
 
   let top_panel = document.getElementById("jp-top-panel");
-  if(top_panel)top_panel.style.display = "none";
+  if (top_panel) top_panel.style.display = "none";
 
   let left_stack = document.getElementById("jp-left-stack");
-  if(left_stack) left_stack.style.display = "none";
+  if (left_stack) left_stack.style.display = "none";
 
   let statusbar = document.getElementById("jp-main-statusbar");
-  if(statusbar) statusbar.style.display = "none";
+  if (statusbar) statusbar.style.display = "none";
 
-  let main_panel= document.getElementById("jp-main-content-panel");
-  if( main_panel ) {
-    if( main_panel.children.length > 0 ){
+  let main_panel = document.getElementById("jp-main-content-panel");
+  if (main_panel) {
+    if (main_panel.children.length > 0) {
       let first_child = main_panel.children[0];
       (first_child as HTMLElement).style.display = "none";
     }
   }
 }
-
-//TODO stopped here
 
 function getUrlUser() {
   let pathArray = window.location.pathname.split("/");
@@ -79,9 +78,11 @@ function getUrlUser() {
  */
 const clearExternalLink = (notebook: NotebookPanel) => {
   const notebookModel = notebook.model;
-  const lastCell = notebookModel.cells.get(notebookModel.cells.length - 1);
-  if (lastCell.id.includes("external")) {
-    console.log("Clearing unused worked example external link.");
+  if (notebookModel) {
+    const lastCell = notebookModel.cells.get(notebookModel.cells.length - 1);
+    if (lastCell.id.includes("external")) {
+      console.log("Clearing unused worked example external link.");
+    }
   }
 };
 
@@ -90,19 +91,23 @@ const generateLinks = (
   docManager: IDocumentManager
 ) => {
   const notebook = notebooks.currentWidget;
-  const notebookModel = notebook.model;
-  const title = notebook.title.label;
+  let title = "";
+  if (notebook) title = notebook.title.label;
 
+  let notebookModel = null;
+  if (notebook) notebookModel = notebook.model;
   let link = "";
   let doesInternalLinkExist = false;
 
-  try {
-    doesInternalLinkExist = notebookModel.cells
-      .get(0)
-      .toJSON()
-      .source[0].includes(viewWeTitle);
-  } catch (err) {
-    console.log(err);
+  if (notebookModel) {
+    try {
+      doesInternalLinkExist = notebookModel.cells
+        .get(0)
+        .toJSON()
+        .source[0].includes(viewWeTitle);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   if (
@@ -116,21 +121,23 @@ const generateLinks = (
       }
     });
     if (title.includes("-gl")) {
-      if (Number.parseInt(urlParams.get("bl")) === 1) {
+      let bl_param = urlParams.get("bl");
+      if (bl_param && Number.parseInt(bl_param) === 1) {
         link = `<span style="font-size:16pt;">[${viewWeTitle}](we-bl-gl.ipynb)</span>`;
       } else {
         link = `<span style="font-size:16pt;">[${viewWeTitle}](we-co-gl.ipynb)</span>`;
       }
     }
     if (title.includes("-na")) {
-      if (Number.parseInt(urlParams.get("bl")) === 1) {
+      let bl_param = urlParams.get("bl");
+      if (bl_param && Number.parseInt(bl_param) === 1) {
         link = `<span style="font-size:16pt;">[${viewWeTitle}](we-bl-na.ipynb)</span>`;
       } else {
         link = `<span style="font-size:16pt;">[${viewWeTitle}](we-co-na.ipynb)</span>`;
       }
     }
-    const markdownModel = new MarkdownCellModel({
-      cell: {
+    if (notebook) {
+      notebook.context.model.sharedModel.addCell({
         cell_type: "markdown",
         source: [link],
         metadata: {
@@ -138,11 +145,8 @@ const generateLinks = (
           editable: false,
         },
         id: "internal-we",
-      },
-    });
-
-    notebookModel.cells.insert(0, markdownModel);
-    isInternalLinkGenerated = true;
+      });
+    }
   }
 
   if (!$("#external-link").length) {
@@ -161,37 +165,40 @@ const generateLinks = (
       let savePromises: Promise<void>[] = [];
       notebooks.forEach((notebook) => {
         let context = docManager.contextForWidget(notebook);
-        let savePromise = context.save();
-        savePromises.push(savePromise);
+        if (context) {
+          let savePromise = context.save();
+          savePromises.push(savePromise);
+        }
       });
       //wait for all save promises
       //qualtrics integration: do not return to hub
-      if( qualtricsParam === "1"){
+      if (qualtricsParam === "1") {
         Promise.all(savePromises).then(() => {
-            //hide the interface
-            //$(".jp-LabShell").hide(); //hidden = true; /
-            document.getElementById("main").style.display = "none"
+          //hide the interface
+          //$(".jp-LabShell").hide(); //hidden = true; /
+          let main = document.getElementById("main")
+          if (main) main.style.display = "none"
 
-            //muck with the history/query params to prevent unhiding via reload
-            urlParams.set("rb", "1");
-            var newRelativePathQuery = window.location.pathname + '?' + urlParams.toString();
-            history.replaceState(null, '', newRelativePathQuery);
+          //muck with the history/query params to prevent unhiding via reload
+          urlParams.set("rb", "1");
+          var newRelativePathQuery = window.location.pathname + '?' + urlParams.toString();
+          history.replaceState(null, '', newRelativePathQuery);
 
-            //reveal instructions in popup since we have no HTML canvas to display on now
-            //Playing around with the idea of keywords; would be easy to cut out
-            let keywords = ["rain", "insure" , "silk" ,"contest", "polish"];
-            let keyword = "";
-            if ( title.includes("we-") ) { keyword = keywords[0]; }
-            else if ( title.includes("ps-near1") ) { keyword = keywords[1]; }
-            else if ( title.includes("ps-near2") ) { keyword = keywords[2]; }
-            else if ( title.includes("ps-far-") ) { keyword = keywords[3]; }
-            else if ( title.includes("ps-farplus") ) { keyword = keywords[4]; }
-            window.alert("The keyword is:\n\n" + keyword + "\n\nPlease enter the keyword in the experiment tab to continue.\n\nClose this window *after* you enter the keyword in the experiment tab.");
-            //no keyword version: window.alert("Please return to the experiment tab to continue.\n\nYou may close this tab now.");
+          //reveal instructions in popup since we have no HTML canvas to display on now
+          //Playing around with the idea of keywords; would be easy to cut out
+          let keywords = ["rain", "insure", "silk", "contest", "polish"];
+          let keyword = "";
+          if (title.includes("we-")) { keyword = keywords[0]; }
+          else if (title.includes("ps-near1")) { keyword = keywords[1]; }
+          else if (title.includes("ps-near2")) { keyword = keywords[2]; }
+          else if (title.includes("ps-far-")) { keyword = keywords[3]; }
+          else if (title.includes("ps-farplus")) { keyword = keywords[4]; }
+          window.alert("The keyword is:\n\n" + keyword + "\n\nPlease enter the keyword in the experiment tab to continue.\n\nClose this window *after* you enter the keyword in the experiment tab.");
+          //no keyword version: window.alert("Please return to the experiment tab to continue.\n\nYou may close this tab now.");
         });
       } else {
         //Wes original functionality
-        Promise.all(savePromises).then(() => window.location.replace(hubLink)); 
+        Promise.all(savePromises).then(() => window.location.replace(hubLink));
       }
     });
     const $notebook = $(".jp-NotebookPanel-notebook");
@@ -221,7 +228,8 @@ const extension: JupyterFrontEndPlugin<void> = {
 
       // Generate notebook links - very specific to DataWhys E1 experiment!
       notebooks.currentChanged.connect(() => {
-        notebooks.currentWidget.context.ready.then(() => {
+        let widget = notebooks.currentWidget;
+        if (widget) widget.context.ready.then(() => {
           generateLinks(notebooks, docManager);
         });
       });
@@ -259,7 +267,8 @@ const extension: JupyterFrontEndPlugin<void> = {
           //Oct 2023: auto render any markdown cells
           // The following builtin method unfortunately steals focus
           // NotebookActions.renderAllMarkdown(notebooks.currentWidget.content)
-          notebooks.currentWidget.content.widgets.forEach((child, index) => {
+          let widget = notebooks.currentWidget;
+          if (widget) widget.content.widgets.forEach((child, index) => {
             if (child.model.type === 'markdown') {
               (child as MarkdownCell).rendered = true;
               child.inputHidden = false;
@@ -271,40 +280,40 @@ const extension: JupyterFrontEndPlugin<void> = {
           //Jan 2022: force every cell of WE and PS1 be attempted in order to allow moving off the notebook
           //NOTE: previous version checked that all code cells were attempted, which was flexible but has the 
           //edge case of them creating a dummy code cell that logically does not need to be filled for completeness
-          if( workedExampleEligible ) {
+          if (workedExampleEligible) {
             //hide the exit link by default for WE and PS1
             $("#external-link").hide();
 
             // Count code cells attempted (non-blank) in the active notebook
             let codeCellAttemptedCount = 0;
 
-            const it = notebooks.currentWidget.model.cells.iter()
-            let result = it.next();
-            while (result !== undefined) {
-              // Count a non-blank code cell
-              if( result.type === 'code' && result.value.text.trim() !== "") {
-                //Oct 2023: check if cell has been executed in addition to being nonempty
-                let codeCell = result as CodeCellModel;
-                if( codeCell.executionCount > 0 )
-                {
-                  codeCellAttemptedCount++;
+            let model = notebooks?.currentWidget?.model;
+            if (model) {
+              for (let j = 0; j < model.cells.length; j++) {
+                // Count a non-blank code cell
+                let result = model.cells.get(j);
+                if (result.type === 'code' && result.sharedModel.source !== "") {
+                  //Oct 2023: check if cell has been executed in addition to being nonempty
+                  let codeCell = result as CodeCellModel;
+                  if (codeCell.executionCount && codeCell.executionCount > 0) {
+                    codeCellAttemptedCount++;
+                  }
+
                 }
-                
               }
-              result = it.next();
             }
-            
-            
+
             // For checking our count, we need to be specific about what notebook we're looking at because they have different #s of code cells
             let allCodeCellsAttempted = false;
-            if( notebooks.currentWidget.title.label.includes("we-") && codeCellAttemptedCount >= 9) { //Was 10 before dataframe display removed 11/22
+            let widget = notebooks.currentWidget;
+            if (widget && widget.title.label.includes("we-") && codeCellAttemptedCount >= 9) { //Was 10 before dataframe display removed 11/22
               allCodeCellsAttempted = true;
-            } else if (notebooks.currentWidget.title.label.includes("near1") && codeCellAttemptedCount >= 5) {
+            } else if (widget && widget.title.label.includes("near1") && codeCellAttemptedCount >= 5) {
               allCodeCellsAttempted = true;
             }
-            
-            
-            if( allCodeCellsAttempted ) {
+
+
+            if (allCodeCellsAttempted) {
               $("#external-link").show();
             }
           }
